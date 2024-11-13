8fa15bcb9df6418b97b9005e620a27eb57db2055
Erfan4708 <abbasy.4708@gmail.com>
2024-11-13 20:44:03 +0330

    Create and implement WebConfig model for storing configs of dll

---
+++ WebApi/Models/WebConfig.cs
@@ -0,0 +1,9 @@
+namespace WebApi.Models
+{
+    public class WebConfig
+    {
+        public string Key { get; set; }
+        public string DefaultValue { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
