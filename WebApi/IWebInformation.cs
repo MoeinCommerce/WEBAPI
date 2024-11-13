84f9d73a81bccbd12492d64cbd9da426e649f0b3
Erfan4708 <abbasy.4708@gmail.com>
2024-11-13 20:45:12 +0330

    Add docstring to IWebInformation interface

--- WebApi/IWebInformation.cs
+++ WebApi/IWebInformation.cs
@@ -1,18 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using WebApi.Models;
 
 namespace WebApi
 {
     public interface IWebInformation
     {
+        /// <summary>
+        /// Gets the name of the dll.
+        /// </summary>
         string Name { get; }
+
+        /// <summary>
+        /// Gets the display name of the dll, typically used in UI contexts.
+        /// </summary>
         string DisplayName { get; }
+
+        /// <summary>
+        /// Gets a brief description of the dll.
+        /// </summary>
         string Description { get; }
+
+        /// <summary>
+        /// Gets the path to the executable file for the dll.
+        /// </summary>
         string ExecutablePath { get; }
+
+        /// <summary>
+        /// Gets the version of the dll file.
+        /// </summary>
         string Version { get; }
-        Dictionary<string, string> Configurations { get; }
+        
+        /// <summary>
+        /// Gets the icon path of the dll.
+        /// </summary>
+        string IconPath { get; }
+
+        /// <summary>
+        /// Gets the configurations of the dll. configurations are used to initialize web connection.
+        /// </summary>
+        List<WebConfig> Configurations { get; }
     }
 }
