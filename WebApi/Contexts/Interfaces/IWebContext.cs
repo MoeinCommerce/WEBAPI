ee2f14e4b710295810001fccbd9ccab1c07885c5
Erfan4708 <abbasy.4708@gmail.com>
2024-11-13 20:46:08 +0330

    Add methods of product and category into IWebContext interface

--- WebApi/Contexts/Interfaces/IWebContext.cs
+++ WebApi/Contexts/Interfaces/IWebContext.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WebApi.Repositories.Entities;
+using WebApi.Models;
 
-namespace WebApi.Contexts
+namespace WebApi.Contexts.Interfaces
 {
     public interface IWebContext
     {
-        IProductRepository WebProductRepository { get; }
-        IOrderRepository WebOrderRepository { get; }
-        ICategoryRepository WebCategoryRepository { get; }
+        #region Product
+
+        WebProduct GetProductById(int id);
+        IEnumerable<WebProduct> GetAllProducts();
+        IEnumerable<WebProduct> GetAllProductsExcludingIds(IList<int> idsToExclude);
+        int CreateProduct(WebProduct entity);
+        int UpdateProduct(int id, WebProduct entity);
+        int GetTotalProductsCount(string searchTerm);
+        IEnumerable<WebProduct> GetAllProductsWithFields(IList<string> fields);
+        IEnumerable<WebProduct> SearchProducts(string searchTerm, int page = 1, int pageSize = 10, int maxPage = 1);
+        int GetMaxProductId();
+        int GetTotalCategoriesCount(string searchTerm);
+
+        #endregion
+
+        #region Category
+        WebCategory GetCategoryById(int id);
+        IEnumerable<WebCategory> GetAllCategories();
+        IEnumerable<WebCategory> GetAllCategoriesExcludingIds(IList<int> idsToExclude);
+        int CreateCategory(WebCategory entity);
+        int UpdateCategory(int id, WebCategory entity);
+        IList<WebCategory> GetAllCategoriesWithFields(IList<string> fields);
+        int GetMaxCategoryId();
+        IList<WebCategory> SearchCategories(string searchTerm, int page = 1, int pageSize = 10, int maxPage = 1);
+
+        #endregion
+
         void Dispose();
     }
 }
