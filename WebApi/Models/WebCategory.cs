7df2b632d4abe816b9a783d94f6d0f7653fb33ab
Erfan4708 <abbasy.4708@gmail.com>
2024-11-15 16:02:11 +0330

    Change parentId type

--- WebApi/Models/WebCategory.cs
+++ WebApi/Models/WebCategory.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int ParentId { get; set; }
+        public int? ParentId { get; set; }
         public string Description { get; set; }
         public string IconPath { get; set; }
     }
