e9582447129377aea2fa9e12f4077a4acdf6e71e
Erfan4708 <abbasy.4708@gmail.com>
2024-12-01 19:03:52 +0330

    Create WebPaymentMethod model

---
+++ WebApi/Models/WebPaymentMethod.cs
@@ -0,0 +1,9 @@
+namespace WebApi.Models
+{
+    public class WebPaymentMethod
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
