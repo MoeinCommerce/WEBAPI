ae5b527c945255edb11820b37ea241a5c029fc74
Erfan4708 <abbasy.4708@gmail.com>
2025-02-21 09:14:46 +0330

    Enhance WebConfig model with additional properties for improved configuration management

--- WebApi/Models/WebConfig.cs
+++ WebApi/Models/WebConfig.cs
@@ -3,7 +3,12 @@ namespace WebApi.Models
     public class WebConfig
     {
         public string Key { get; set; }
-        public string DefaultValue { get; set; }
         public string DisplayName { get; set; }
+        public string DefaultValue { get; set; }
+        public string Value { get; set; }
+        public bool IsRequired { get; set; }
+        public bool IsReadOnly { get; set; }
+        public bool IsProtected { get; set; }
+        public string Description { get; set; }
     }
 }
