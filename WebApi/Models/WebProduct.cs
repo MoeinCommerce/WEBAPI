8d37707036c4bc0211081e8738e5f9b4ae820381
Erfan4708 <abbasy.4708@gmail.com>
2025-03-13 09:50:29 +0330

    feat: add attribute model

--- WebApi/Models/WebProduct.cs
+++ WebApi/Models/WebProduct.cs
@@ -13,7 +13,6 @@ namespace WebApi.Models
         public string Slug { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
-        // public string Status { get; set; }
         public string Description { get; set; }
         public string Sku { get; set; }
         public decimal? BuyPrice { get; set; }
@@ -23,5 +22,13 @@ namespace WebApi.Models
         public List<WebCategory> Categories { get; set; }
         public string IconPath { set; get; }
         public string Type { get; set; }
+        public List<Attribute> Attributes { get; set; }
+
+    }
+    public class Attribute
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
     }
 }
