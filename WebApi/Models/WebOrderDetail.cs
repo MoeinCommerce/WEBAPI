bbe219b0147440604efb08c23f915baea5f14ce6
Erfan4708 <abbasy.4708@gmail.com>
2025-04-22 19:35:43 +0330

    fix: change Quantity property type to decimal

--- WebApi/Models/WebOrderDetail.cs
+++ WebApi/Models/WebOrderDetail.cs
@@ -6,7 +6,7 @@ namespace WebApi.Models
         public string Name { get; set; }
         public int ProductId { get; set; }
         public int VariationId { get; set; }
-        public int Quantity { get; set; }
+        public double Quantity { get; set; }
         public double UnitPrice { get; set; }
         public double UnitDiscount { get; set; }
         public double UnitTax { get; set; }
