3421ece0238bea08bc5e56ffcbeffba915b6302b
Erfan4708 <abbasy.4708@gmail.com>
2025-07-06 09:39:12 +0330

    feat: add ExceptionFields class for standardized exception messages

---
+++ WebApi/Exceptions/ExceptionFields.cs
@@ -0,0 +1,24 @@
+namespace WebApi.Exceptions
+{
+    public static class ExceptionFields
+    {
+        public const string DuplicateCategoryName = "DuplicateCategoryName";
+        public const string InvalidCategoryId = "InvalidCategoryId";
+        public const string InvalidCategoryName = "InvalidCategoryName";
+        public const string MissingParentCategoryId = "MissingParentCategoryId";
+        public const string InvalidProductCode = "InvalidProductCode";
+        public const string InvalidProductNameLength = "InvalidProductNameLength";
+        public const string InvalidProductDescriptionLength = "InvalidProductDescriptionLength";
+        public const string InvalidSku = "InvalidSku";
+        public const string InvalidQuantity = "InvalidQuantity";
+        public const string InvalidPrice = "InvalidPrice";
+        public const string InvalidProductCategory = "InvalidProductCategory";
+        public const string OrderItemNotFound = "OrderItemNotFound";
+        public const string WarehouseIdNotFound = "WarehouseIdNotFound";
+        public const string PaymentMethodNotFound = "PaymentMethodNotFound";
+        public const string CustomerNotFound = "CustomerNotFound";
+        public const string InvalidOrderItemQuantity = "InvalidOrderItemQuantity";
+        public const string InvalidOrderItemPrice = "InvalidOrderItemPrice";
+        public const string InvalidTotalOrderPrice = "InvalidTotalOrderPrice";
+    }
+}
